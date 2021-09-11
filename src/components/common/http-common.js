6dd239e368680bcf6f813f37cb459cc31f7a7fa9
Olayinka <holahyeankah12@gmail.com>
2021-09-11 09:08:32 +0100

    changing url

--- src/components/common/http-common.js
+++ src/components/common/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const token=localStorage.getItem(`${process.env.SECRET_KEY}`)
 
 export default axios.create({
-    baseURL:`${process.env.REACT_APP_API_URL}`,
+    baseURL:`https://tutoriaal.herokuapp.com`,
     headers:{
         "content-type":"application/json",'Authorization':`Bearer ${token}`
 
